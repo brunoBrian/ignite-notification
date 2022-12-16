1e60ef4c1222a6a33989867e3092a33454cf009c
brunoBrian <bruninho_brian@hotmail.com>
2022-12-15 22:22:39 -0300

    create test in notification entity

--- src/application/entities/notification.spec.ts
+++ src/application/entities/notification.spec.ts
@@ -1,6 +1,6 @@
 import { Content } from "./content"
 
-describe('Notification content', () => {
+describe('Notification', () => {
   it('should be able to create a notification content', () => {
     const content = new Content('Você recebeu uma solicitação de amizade.');
 
